73d895aa029022b24ef7c57486620bf23b0e608c
yves.prelot <yves.prelot@gmail.com>
2013-09-23 14:15:20 +0200

    Reset

--- Parser/Parser/src/parser/px/analyzers/predefs/CompactStructuredAnalyzer.scala
+++
@@ -1,7 +0,0 @@
-package parser.px.analyzers.predefs
-import parser.px.Analyzer
-import parser.px.analyzers.{NoFilter,StructAnalyzer}
-import parser.Source
-import parser.px.analyzers.structured.predefs.CompactField
-
-object CompactStructuredAnalyzer extends Analyzer with StructAnalyzer with CompactField with NoFilter
--- Parser/Parser/src/parser/px/analyzers/predefs/SimpleStructuredAnalyzer.scala
+++
@@ -1,6 +0,0 @@
-package parser.px.analyzers.predefs
-import parser.px.Analyzer
-import parser.px.analyzers.{NoFilter,StructAnalyzer}
-import parser.px.analyzers.structured.predefs.SimpleField
-
-object SimpleStructuredAnalyzer extends Analyzer with StructAnalyzer with SimpleField with NoFilter
