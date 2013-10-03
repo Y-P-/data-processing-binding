88b29ece5bd48800c174cf3e8f6d711b5666420b
yves.prelot <yves.prelot@gmail.com>
2013-10-03 17:25:06 +0200

    CLOSED - task 2: Enhanced Processor https://github.com/Y-P-/data-processing-binding/issues/issue/2

--- XX3/src/loader/motors/Struct.scala
+++ XX3/src/loader/motors/Struct.scala
@@ -13,18 +13,11 @@ import loader.core.ParserBuilder
 
 
 object Struct extends Processor {self=>
-  /** 
-   * @param tabs the current tabulation value
-   * @param written is true if the starting tag has been written
-   */
-  class Data(val tabs:Int,var written:Boolean) {
-    def this(tabs:Int) = this(tabs,false)
-  }
   
   trait DefImpl extends loader.core.ExtCore.Def {
     type Kind    = String
     type Ret     = Unit
-    type Data    = Struct.Data
+    type Data    = Null
     type Parser  = ParserBuilder#Impl  //any parser
     def kindClass = classOf[Kind]
     def parsClass = classOf[Parser]
@@ -47,23 +40,16 @@ object Struct extends Processor {self=>
       // - onVal   => we have a valued element: it begins as     name =
       // - onChild => we have a container element: it begins as  name = {
       // - onEnd   => we have an empty element: it begins as     name = {
-      //also note that we must act on the first such call, otherwise the call will write data before we write the container tag!
-      //this means that we cannot act with onChild (too late: sub data has been written already) ; this only leaves the onBeg method
-      //of any child to write the starting tag of its parent, while the onVal can account for a valued element.
-      //This has the nasty side effect that unlike what is expected, onBeg is not acting on the current element, but its parent! 
-      //This is not expected by the framework and we must correct this misbeheavior here too ; we do this by testing e.parent!=null
-      //to avoid unwanted output on the top element.
-      protected def getData(parent:Element,s:Status):Data = new Data((if (parent!=null) parent.data.tabs+indent else -indent))
+      protected def getData(parent:Element,s:Status):Data = null
 
-      //XXX  use a qName formatter
-      private def newLine(e:Element):Unit   = out.write(if (indent>0) Indent(e.data.tabs) else " ")
-      private def tag(e:Element):Unit       = { newLine(e); if (!e.name.isEmpty) { quote(e.localName); out.write(s" = ") } }
+      private def newLine(e:Element):Unit   = out.write(if (indent>0) Indent((e.depth-1)*indent) else " ")
       private def quote(s:String):Unit      = out.write(if(parser.px.Data.isChar(s)) s else s""""$s"""")
-      private def writeBeg(e:Element):Unit  = if (e!=null && e.parent!=null && !e.data.written) { tag(e); out.write("{"); e.data.written=true }
+      private def tag(e:Element):Unit       = { newLine(e); if (!e.name.isEmpty) { quote(e.localName); out.write(s" = ") } }
   
-      protected def onBeg(e:Element): Unit           = writeBeg(e.parent)
-      protected def onVal(e:Element,v: String): Ret  = { tag(e); quote(v); out.flush }
-      protected def onEnd(e:Element): Ret            = if (e.parent!=null) { newLine(e); out.write("}"); out.flush }
+      protected def onInit(e:Element):Unit          = {}
+      protected def onBeg(e:Element): Unit          = if (e.parent!=null) { tag(e); out.write("{") }
+      protected def onVal(e:Element,v: String): Ret = { tag(e); quote(v); out.flush }
+      protected def onEnd(e:Element): Ret           = if (e.parent!=null) { newLine(e); out.write("}"); out.flush }
       protected def onChild(e:Element,child: Element, r: Ret): Unit = {}
 
       protected def onInit():Unit = {}
@@ -98,7 +84,7 @@ object Struct extends Processor {self=>
 
 abstract class Processor {
   import loader.core._
-  abstract class Motor[E<:definition.Def#Elt](val userCtx:UserContext[E])
+  abstract class Motor[E<:definition.Def#Element](val userCtx:UserContext[E])
   val ctx:CtxCore.Impl
   val ext:ExtCore.Impl
 }
