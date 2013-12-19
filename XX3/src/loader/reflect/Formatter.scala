8ce9fbcc889b7b396b0bf9d3673b8812903f1f3a
yves.prelot <yves.prelot@gmail.com>
2013-12-19 18:22:37 +0100

    current work

--- XX3/src/loader/reflect/Formatter.scala
+++ XX3/src/loader/reflect/Formatter.scala
@@ -13,6 +13,39 @@ trait FormatData {
   def fmt:String
 }
 
-class FmtInt[-E<:Def#Elt,+D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData) extends Formatter[Integer,E,E1] {
-  def apply(x:Integer,e:E,e1:E1) = e1.pull(String.format(fd.fmt,x))
+trait FmtBuilder[X,-E<:Def#Elt] {
+  def apply[D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData):Formatter[X,E,E1]
+}
+
+object Formatters {
+  
+  protected class IntFormatter[D1<:Def{ type Kind>:String },E1<:D1#Elt](fmt:String) {
+    def apply(x:Integer,e:Def#Elt,e1:E1) = e1.pull(String.format(fmt,x))    
+  }
+
+  object FmtJInt extends FmtBuilder[Integer,Def#Elt] {
+    def apply[D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData) = new Formatter[Integer,Def#Elt,E1] {
+      val fmt = if (fd.fmt==null || fd.fmt.length==0) "%d" else fd.fmt
+      def apply(x:Integer,e:Def#Elt,e1:E1) = e1.pull(String.format(fmt,x))
+    }
+  }
+  object FmtInt extends FmtBuilder[Int,Def#Elt] {
+    def apply[D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData) = new Formatter[Int,Def#Elt,E1] {
+      val fmt = if (fd.fmt==null || fd.fmt.length==0) "%d" else fd.fmt
+      def apply(x:Int,e:Def#Elt,e1:E1) = e1.pull(String.format(fmt,new Integer(x)))
+    }
+  }
+  object FmtJShort extends FmtBuilder[java.lang.Short,Def#Elt] {
+    def apply[D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData) = new Formatter[java.lang.Short,Def#Elt,E1] {
+      val fmt = if (fd.fmt==null || fd.fmt.length==0) "%d" else fd.fmt
+      def apply(x:java.lang.Short,e:Def#Elt,e1:E1) = e1.pull(String.format(fmt,x))
+    }
+  }
+  object FmtShort extends FmtBuilder[Short,Def#Elt] {
+    def apply[D1<:Def{ type Kind>:String }, E1<:D1#Elt](fd:FormatData) = new Formatter[Short,Def#Elt,E1] {
+      val fmt = if (fd.fmt==null || fd.fmt.length==0) "%d" else fd.fmt
+      def apply(x:Short,e:Def#Elt,e1:E1) = e1.pull(String.format(fmt,new java.lang.Short(x)))
+    }
+  }
+
 }
