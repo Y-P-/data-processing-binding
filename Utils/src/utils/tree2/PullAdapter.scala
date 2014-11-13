275c0d0dfa24ed06b8eeca96b8d0d4f12d63e31c
Y-P- <yves.prelot@free.fr>
2014-11-13 23:45:53 +0100

    Replaced BlockingQueue by blocking single element

--- Utils/src/utils/tree2/PullAdapter.scala
+++ Utils/src/utils/tree2/PullAdapter.scala
@@ -9,12 +9,15 @@ import scala.concurrent._
  */
 class PullAdapter[K<:AnyRef,V] extends PushPull[K,V] {
 
+  /** Used to turn the push/pull into a PrefixTraversableOnce.
+   *  The 'once' is not to trifle with: even 'hasNext' cannot be called more than once per element!
+   */
   class Layer extends Iterator[(K,Layer)] with PrefixTraversableOnce[K,V,Layer] {
-    var next:(K,Layer) = _
+    var next:(K,Layer)  = _
     var value:Option[V] = None
-    @tailrec final def hasNext:Boolean = queue.take() match {
+    @tailrec final def hasNext:Boolean = item.take match {
         case PullAdapter.End => false
-        case o:Option[V]     => if (value!=None) throw new IllegalStateException("the same object cannot receive two values")
+        case o:Option[V]     => if (value.isDefined) throw new IllegalStateException("the same element cannot receive two values")
                                 value = o
                                 hasNext
         case k:K             => next = (k,new Layer)
@@ -22,11 +25,11 @@ class PullAdapter[K<:AnyRef,V] extends PushPull[K,V] {
       }    
   }  
   
-  val queue = new java.util.concurrent.ArrayBlockingQueue[AnyRef](1)
+  protected val item = new PullAdapter.ItemLock[AnyRef]
   
-  final def push(k:K) = queue.put(k)
-  final def pull(v:V) = queue.put(Some(v))
-  final def pull      = queue.put(PullAdapter.End)
+  final def push(k:K) = item.put(k)
+  final def pull(v:V) = item.put(Some(v))
+  final def pull      = item.put(PullAdapter.End)
   
   def run[U](x: =>Unit)(op: ((K,Layer),Iterator[U]) => U) = {
     import scala.concurrent._
@@ -39,6 +42,39 @@ class PullAdapter[K<:AnyRef,V] extends PushPull[K,V] {
 
 object PullAdapter {
   object End
+  
+  class ItemLock[E>:Null] {
+    import java.util.concurrent.locks.Condition;
+    import java.util.concurrent.locks.ReentrantLock;
+    var item:E = null
+    val lock = new ReentrantLock(false)
+    val notEmpty = lock.newCondition
+    val notFull =  lock.newCondition
+    def take:E = {
+      lock.lockInterruptibly
+      try {
+        while (item == null)
+          notEmpty.await
+        val x = item
+        item = null
+        notFull.signal
+        x
+      } finally {
+        lock.unlock
+      }
+    }
+    def put(e:E):Unit = {
+      lock.lockInterruptibly
+      try {
+        while (item != null)
+          notFull.await
+        item = e;
+        notEmpty.signal
+      } finally {
+        lock.unlock
+      }
+    }
+  }
   
   def main(args:Array[String]):Unit = {
     val s = new PullAdapter[String,String]
--- Utils/src/utils/tree2/PushPull.scala
+++ Utils/src/utils/tree2/PushPull.scala
@@ -3,7 +3,7 @@ package utils.tree2
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-abstract class PushPull[-K,+V] {
+trait PushPull[-K,+V] {
   def push(key:K):Unit
   protected[this] def pull(value:V):Unit
   def pull():Unit
